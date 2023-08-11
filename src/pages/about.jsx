90a6b6001dfba8a2b22dd152c493bf8961167cd4
Mikolaj Greszta <MG246981@falmouth.ac.uk>
2023-08-11 08:25:51 +0100

    Added phone number

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -37,6 +37,18 @@ function MailIcon(props) {
   )
 }
 
+function PhoneIcon(props)
+{
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+      />
+    </svg>
+  )
+}
+
 //mention game master in dnd
 
 export default function About() {
@@ -108,6 +120,10 @@ export default function About() {
               >
                 mikolajgreszta@gmail.com
               </SocialLink>
+              <SocialLink
+              href={""}
+              icon={PhoneIcon}>
+                07702 763785</SocialLink>
             </ul>
           </div>
         </div>
