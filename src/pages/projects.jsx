64eb907abf964a22b59fbcc81c22a424a1f72bd4
mikboiii <mikolajgreszta@gmail.com>
2026-03-03 11:20:13 +0000

    Added other projects section

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -11,6 +11,8 @@ import caliCatSplash from 'public/images/photos/calicatSplash.png'
 import butterDogs from 'public/images/logos/butterdogs.png'
 import myceliaSplash from 'public/images/photos/MyceliaSplash.png'
 import nightfallSplash from 'public/images/photos/nightfallSplash.png'
+import dissertationSplash from 'public/images/photos/DissertationSplash.png'
+import portraitIcon from 'public/images/portrait.jpg'
 
 const posts = [
 
@@ -78,6 +80,25 @@ const personalPosts = [
       name: 'Voxflare',
       imageUrl:
       nightfallSplash,
+    }
+  }
+]
+
+const otherPosts = [
+  {
+        id: 1,
+    title: 'Observing the Relationship Between Melodic Traits and their Effect on the Suitability of a Generated Melody',
+    href: 'MGDissertation.pdf',
+    description:
+      'A dive into desirable elements in procedural music',
+    imageUrl:
+    dissertationSplash,
+    date: '31st March 2023',
+    datetime: '2023-03-31',
+    author: {
+      name: 'Mikolaj Greszta',
+      imageUrl:
+      portraitIcon,
     }
   }
 ]
@@ -180,7 +201,47 @@ export default function Projects() {
         </div>
       </div>
     </div>
+    <div className="mx-auto max-w-2xl text-center py-8">
+          <h2 className="text-3xl font-bold tracking-tight text-black dark:text-white sm:text-4xl">Other Projects</h2>
+          <p className="mt-2 text-lg leading-8 text-black dark:text-white">
+            These are the projects which I&apos;ve worked on but aren&apos;t necessarily games related
+          </p>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-0 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {otherPosts.map((post) => (
+            <article
+              key={post.id}
+              className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80 border-solid border-4 border-transparent transition ease-in-out hover:border-teal-500"
+            >
+              <Image src={post.imageUrl} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
+              <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+              <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+
+              <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
+                <time dateTime={post.datetime} className="mr-8">
+                  {post.date}
+                </time>
+                <div className="-ml-4 flex items-center gap-x-4">
+                  <svg viewBox="0 0 2 2" className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
+                    <circle cx={1} cy={1} r={1} />
+                  </svg>
+                  <div className="flex gap-x-2.5">
+                    <Image src={post.author.imageUrl} alt="" className="h-6 w-6 flex-none rounded-full bg-white/10" />
+                    {post.author.name}
+                  </div>
+                </div>
+              </div>
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
+                <a href={post.href}>
+                  <span className="absolute inset-0" />
+                  {post.title}
+                </a>
+              </h3>
+            </article>
+          ))}
+        </div>
       </SimpleLayout>
+      
     </>
   )
 }
